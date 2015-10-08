a7b4d937874a24d7f2f39b85c67efda226d48f7a
Axel Ramos <aramos-2014171@kinal.edu.gt>
2015-10-08 16:12:39 -0600

    haciendo la vista de courso de agregar y eleminar

---
+++ EntityDatabase/Modules/Courses/Views/CreatedCourse.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports Modules.Courses.ViewModels
+Public Class CreatedCourse
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.Grid.DataContext = New CreatedCourseViewModel(Me)
+    End Sub
+End Class
